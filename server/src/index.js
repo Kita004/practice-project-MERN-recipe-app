a4128cf6e1e5f7ee080c166d6f4e14cea0da3e0f
Kitti Born <born.kitti97@gmail.com>
2023-05-17 11:29:53 +0200

    connect to DB

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.ATLAS_URI);
 
 app.listen(3001, () => console.info("SERVER STARTED!"));

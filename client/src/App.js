1763b419eeb79a8e2ffb97850894410b2d475174
Kitti Born <born.kitti97@gmail.com>
2023-05-17 14:16:52 +0200

    add Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { AuthPage } from "./pages/AuthPage";
+import { RecipeFormPage } from "./pages/RecipeFormPage";
+import { RecipeList } from "./pages/RecipeList";
 
 function App() {
-    return <div className="App">Hello World!</div>;
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/auth" element={<AuthPage />} />
+                    <Route path="/create-recipe" element={<RecipeFormPage />} />
+                    <Route path="/recipes" element={<RecipeList />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;

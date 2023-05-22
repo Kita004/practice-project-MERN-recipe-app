cfd7db9d6ccd4b2e48a7f6c5278f073cf9f70707
Kitti Born <born.kitti97@gmail.com>
2023-05-22 11:08:00 +0200

    add feature for logging in user

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export const AuthPage = () => {
@@ -14,9 +16,32 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const [_, setCookies] = useCookies(["access_token"]);
+    const nav = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post(
+                "http://localhost:3001/auth/login",
+                {
+                    username,
+                    password,
+                }
+            );
+            setCookies("access_token", response.data.token);
+            window.localStorage.setItem("userID", response.data.userID);
+
+            nav("/");
+        } catch (err) {
+            console.info("Logged in Successfully!");
+        }
+    };
+
     return (
         <Form
             label={"Login"}
+            handleSubmit={handleSubmit}
             username={username}
             setUsername={setUsername}
             password={password}

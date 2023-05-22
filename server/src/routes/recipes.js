db084d6f048c55fcb702d2451bfb4b937b57f0df
Kitti Born <born.kitti97@gmail.com>
2023-05-22 12:42:07 +0200

    add logic for recipe route

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -20,6 +21,41 @@ router.post("/", async (req, res) => {
         res.json(response);
     } catch (err) {
         return res.json(err);
+    }
+});
+
+router.put("/", async (req, res) => {
+    try {
+        const recipe = await RecipeModel.findById(req.body.recipeId);
+        const user = await UserModel.findById(req.body.userId);
+
+        user.savedRecipes.push(recipe);
+        await user.save();
+
+        res.json({ savedRecipes: user.savedRecipes });
+    } catch (err) {
+        return res.json(err);
+    }
+});
+
+router.get("/saved-recipes/ids", async () => {
+    try {
+        const user = await UserModel.findById(req.body.userId);
+        res.json({ savedRecipes: user?.savedRecipes });
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+router.get("/saved-recipes", async () => {
+    try {
+        const user = await UserModel.findById(req.body.userId);
+        const savedRecipes = await RecipeModel.find({
+            _id: { $in: user.savedRecipes },
+        });
+        res.json({ savedRecipes });
+    } catch (err) {
+        res.json(err);
     }
 });
 

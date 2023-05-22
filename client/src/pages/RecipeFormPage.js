adf4a99c03dac02952637aa4079c672ed69c6092
Kitti Born <born.kitti97@gmail.com>
2023-05-22 12:49:02 +0200

    add UI for Recipe Form

--- client/src/pages/RecipeFormPage.js
+++ client/src/pages/RecipeFormPage.js
@@ -1,3 +1,27 @@
 export const RecipeFormPage = () => {
-    return <div>This is The Form for creating Recipes!</div>;
+    return (
+        <div className="create-recipe">
+            <h2>Create Recipe!</h2>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+
+                <label htmlFor="description">Description</label>
+                <textarea id="description" name="description"></textarea>
+
+                <label htmlFor="ingredients">Ingredients</label>
+
+                <label htmlFor="instructions">Instructions</label>
+                <textarea id="instructions" name="instructions"></textarea>
+
+                <label htmlFor="imageUrl">Image URL</label>
+                <input type="text" id="imageUrl" name="imageUrl" />
+
+                <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+                <input type="number" id="cookingTime" name="cookingTime" />
+
+                <button type="submit">Create Recipe</button>
+            </form>
+        </div>
+    );
 };

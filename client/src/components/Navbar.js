a77a61824163a9ab8b640709ef6f41d8c9e765f9
Kitti Born <born.kitti97@gmail.com>
2023-05-22 11:21:30 +0200

    add feature for logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,26 @@
+import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const nav = useNavigate();
+
+    const logout = () => {
+        setCookies("access_token", "");
+        window.localStorage.clear();
+        nav("/auth");
+    };
     return (
         <nav className="navbar">
             <Link to="/">Home</Link>
             <Link to="/create-recipe">Create Recipe</Link>
             <Link to="/recipes">Recipes</Link>
-            <Link to="/auth">Register/Login</Link>
+            {cookies.access_token ? (
+                <button onClick={() => logout()}>Logout</button>
+            ) : (
+                <Link to="/auth">Register/Login</Link>
+            )}
         </nav>
     );
 };

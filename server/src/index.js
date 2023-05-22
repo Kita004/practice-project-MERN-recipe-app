24274d741242036eb54f5a14f941429f1e98cac2
Kitti Born <born.kitti97@gmail.com>
2023-05-22 12:22:47 +0200

    implement features for listing all recipes and creating new ones

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import { userRouter } from "./routes/users.js";
+import { recipeRouter } from "./routes/recipes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", userRouter);
+app.use("/recipes", recipeRouter);
 
 mongoose.connect(process.env.ATLAS_URI);
 
